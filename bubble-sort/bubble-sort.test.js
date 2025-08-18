54a79bf47d6963b775d6b90911380b266ac45954
BYCODDE <116018979+BYCODDE@users.noreply.github.com>
2025-08-18 17:10:31 +0400

    bubble sort test add

--- bubble-sort/bubble-sort.test.js
+++ bubble-sort/bubble-sort.test.js
@@ -1,11 +1,24 @@
 function bubbleSort(nums) {
-  
+  let swapped = false;
+
+  do {
+    swapped = false;
+
+    for (let i = 0; i < nums.length - 1; i++) {
+      if (nums[i] > nums[i + 1]) {
+        const temp = nums[i];
+        nums[i] = nums[i + 1];
+        nums[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+
+  return nums;
 }
 
-
-test('bubbleSort', function () {
-
-  const nums = [4, 2, 1, 3];
+test("bubbleSort", function () {
+  const nums = [4, 2, 1, 3, 11];
   const sortedArray = bubbleSort(nums);
-  expect(sortedArray).toEqual([1, 2, 3, 4]);
-})
+  expect(sortedArray).toEqual([1, 2, 3, 4, 11]);
+});
